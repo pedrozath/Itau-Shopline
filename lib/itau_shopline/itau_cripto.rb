f7f1bc0c8f778005fd59c77a4c3427880c6f8814
Pedro Maciel <pedrozath@gmail.com>
2013-04-07 03:35:18 -0300

    updates

--- lib/itau_shopline/itau_cripto.rb
+++ lib/itau_shopline/itau_cripto.rb
@@ -17,68 +17,69 @@ class ItauCripto
     @dados = nil
   end
   
-  def gera_dados(param_string1, param_string2, param_string3, param_string4, param_string5, param_string6, param_string7, param_string8, param_string9, param_string10, param_string11, param_string12, param_string13, param_string14, param_string15, param_string16, param_string17, param_string18)
-    param_string1 = param_string1.upcase
-    param_string5 = param_string5.upcase
+  def gera_dados(param_string)
+    
+    param_string[0] = param_string[0].upcase
+    param_string[4] = param_string[4].upcase
 
   
-    return "Erro: tamanho do codigo da empresa diferente de 26 posições." if param_string1.size != TAM_COD_EMP
-    return "Erro: tamanho da chave da chave diferente de 16 posições." if param_string5.size != TAM_CHAVE
-    return "Erro: número do pedido inválido." if param_string2.size < 1 || param_string2.size > 8
+    return "Erro: tamanho do codigo da empresa diferente de 26 posições." if param_string[0].size != TAM_COD_EMP
+    return "Erro: tamanho da chave da chave diferente de 16 posições." if param_string[4].size != TAM_CHAVE
+    return "Erro: número do pedido inválido." if param_string[1].size < 1 || param_string[1].size > 8
     
-    if is_numeric(param_string2)
-      param_string2 = preenche_zero(param_string2, 8)
+    if is_numeric(param_string[1])
+      param_string[1] = preenche_zero(param_string[1], 8)
     else
       return "Erro: numero do pedido não é numérico."
     end
       
-    return "Erro: valor da compra inválido." if param_string3.size < 1 || param_string3.size > 11
+    return "Erro: valor da compra inválido." if param_string[2].size < 1 || param_string[2].size > 11
 
-    i = param_string3 =~ /,/
+    i = param_string[2] =~ /,/
     if i
-      str3 = param_string3[(i + 1)..-1]
+      str3 = param_string[2][(i + 1)..-1]
     
       return "Erro: valor decimal não é numérico." if !is_numeric(str3)
       return "Erro: valor decimal da compra deve possuir 2 posições após a virgula." if str3.size != 2
       
-      param_string3 = param_string3[0, (param_string3.size - 3)] + str3
+      param_string[2] = param_string[2][0, (param_string[2].size - 3)] + str3
     else
-      return "Erro: valor da compra não é numérico." if !is_numeric(param_string3)
-      return "Erro: valor da compra deve possuir no máximo 8 posições antes da virgula." if param_string3.size > 8
+      return "Erro: valor da compra não é numérico." if !is_numeric(param_string[2])
+      return "Erro: valor da compra deve possuir no máximo 8 posições antes da virgula." if param_string[2].size > 8
 
-      param_string3 = param_string3 + "00"
+      param_string[2] = param_string[2] + "00"
     end
     
-    param_string3 = preenche_zero(param_string3, 10)
+    param_string[2] = preenche_zero(param_string[2], 10)
 
-    param_string7 = param_string7.strip
+    param_string[6] = param_string[6].strip
       
       
-    return "Erro: código de inscrição inválido." if param_string7 != "02" && param_string7 != "01" && param_string7 != ""
-    return "Erro: número de inscrição inválido." if param_string8 != "" && !is_numeric(param_string8) && param_string8.size > 14
-    return "Erro: cep inválido." if param_string11 != "" && !is_numeric(param_string11) || param_string11.size != 8
-    return "Erro: data de vencimento inválida." if param_string14 != "" && !is_numeric(param_string14) || param_string14.size != 8
-    return "Erro: observação adicional 1 inválida." if param_string16.size > 60
-    return "Erro: observação adicional 2 inválida." if param_string17.size > 60
-    return "Erro: observação adicional 3 inválida." if param_string18.size > 60
+    return "Erro: código de inscrição inválido." if param_string[6] != "02" && param_string[6] != "01" && param_string[6] != ""
+    return "Erro: número de inscrição inválido." if param_string[7] != "" && !is_numeric(param_string[7]) && param_string[7].size > 14
+    return "Erro: cep inválido." if param_string[10] != "" && !is_numeric(param_string[10]) || param_string[10].size != 8
+    return "Erro: data de vencimento inválida. #{param_string[13]}" if param_string[13] != "" && !is_numeric(param_string[13]) || param_string[13].size != 8
+    return "Erro: observação adicional 1 inválida." if param_string[15].size > 60
+    return "Erro: observação adicional 2 inválida." if param_string[16].size > 60
+    return "Erro: observação adicional 3 inválida." if param_string[17].size > 60
 
-    param_string4  = preenche_branco(param_string4, 40)
-    param_string6  = preenche_branco(param_string6, 30)
-    param_string7  = preenche_branco(param_string7, 2)
-    param_string8  = preenche_branco(param_string8, 14)
-    param_string9  = preenche_branco(param_string9, 40)
-    param_string10 = preenche_branco(param_string10, 15)
-    param_string11 = preenche_branco(param_string11, 8)
-    param_string12 = preenche_branco(param_string12, 15)
-    param_string13 = preenche_branco(param_string13, 2)
-    param_string14 = preenche_branco(param_string14, 8)
-    param_string15 = preenche_branco(param_string15, 60)
-    param_string16 = preenche_branco(param_string16, 60)
-    param_string17 = preenche_branco(param_string17, 60)
-    param_string18 = preenche_branco(param_string18, 60)
+    param_string[3]  = preenche_branco(param_string[3], 40)
+    param_string[5]  = preenche_branco(param_string[5], 30)
+    param_string[6]  = preenche_branco(param_string[6], 2)
+    param_string[7]  = preenche_branco(param_string[7], 14)
+    param_string[8]  = preenche_branco(param_string[8], 40)
+    param_string[9] = preenche_branco(param_string[9], 15)
+    param_string[10] = preenche_branco(param_string[10], 8)
+    param_string[11] = preenche_branco(param_string[11], 15)
+    param_string[12] = preenche_branco(param_string[12], 2)
+    param_string[13] = preenche_branco(param_string[13], 8)
+    param_string[14] = preenche_branco(param_string[14], 60)
+    param_string[15] = preenche_branco(param_string[15], 60)
+    param_string[16] = preenche_branco(param_string[16], 60)
+    param_string[17] = preenche_branco(param_string[17], 60)
 
-    str1 = algoritmo(param_string2 + param_string3 + param_string4 + param_string6 + param_string7 + param_string8 + param_string9 + param_string10 + param_string11 + param_string12 + param_string13 + param_string14 + param_string15 + param_string16 + param_string17 + param_string18, param_string5)
-    str2 = algoritmo(param_string1 + str1, CHAVE_ITAU)
+    str1 = algoritmo(param_string[1] + param_string[2] + param_string[3] + param_string[4] + param_string[6] + param_string[7] + param_string[8] + param_string[9] + param_string[10] + param_string[11] + param_string[12] + param_string[13] + param_string[14] + param_string[15] + param_string[16] + param_string[17], param_string[4])
+    str2 = algoritmo(param_string[0] + str1, CHAVE_ITAU)
     
     converte(str2)
   end
--- lib/itau_shopline/itau_shopline.rb
+++ lib/itau_shopline/itau_shopline.rb
@@ -6,97 +6,123 @@ require "rexml/document"
 require "open-uri"
 
 class ItauShopline
-  include ActionView::Helpers::NumberHelper
+	include ActionView::Helpers::NumberHelper
 
-  STATUS_TRANSLATE = {
-    :pending => 'Pendente',
-    :paid => 'Pago'
-  }
-  
-  PAYMENT_TRANSLATE = {
-    :undefined => 'Não definido',
-    :online_transfer => 'Transferência',
-    :invoice => 'Boleto',
-    :credit_card => 'Cartão de crédito'
-  }
+	STATUS_TRANSLATE = {
+		:pending => 'Pendente',
+		:paid => 'Pago'
+	}
+	
+	PAYMENT_TRANSLATE = {
+		:undefined => 'Não definido',
+		:online_transfer => 'Transferência',
+		:invoice => 'Boleto',
+		:credit_card => 'Cartão de crédito'
+	}
 
-  STATUS = {
-    :undefined => {
-      '01' => :pending,
-      '02' => :pending,
-      '03' => :pending
-    },
-    :online_transfer => {
-      '00' => :paid,
-      '01' => :pending,
-      '02' => :pending,
-      '03' => :pending      
-    },
-    :invoice => {
-      '00' => :paid,
-      '01' => :pending,
-      '02' => :pending,
-      '03' => :pending,
-      '04' => :pending,
-      '05' => :paid,
-      '06' => :pending   
-    },
-    :credit_card => {
-      '00' => :paid,
-      '01' => :pending,
-      '02' => :pending,
-      '03' => :pending      
-    }
-  }
-  
-  PAYMENT_METHODS = {
-    '00' => :undefined,
-    '01' => :online_transfer,
-    '02' => :invoice,    
-    '03' => :credit_card
-  }
-  
-  def gera_dados(invoice_id, total_price, full_name, city_name, state_uf, due_date=nil, return_url="")
-    cripto = ItauCripto.new
-    due_date ||= (Date.today + 3.days).strftime('%d%m%Y')
-    cripto.gera_dados(config['codigo_empresa'], invoice_id.to_s, number_to_currency(total_price, :unit => "", :separator => ",", :delimiter => ""), "",  config['chave'], full_name, '01', '00000000000', '', '', "00000000", city_name[0,15], state_uf[0,2], due_date, return_url[0,60], '', '', '')
-  end
+	STATUS = {
+		:undefined => {
+			'01' => :pending,
+			'02' => :pending,
+			'03' => :pending
+		},
+		:online_transfer => {
+			'00' => :paid,
+			'01' => :pending,
+			'02' => :pending,
+			'03' => :pending      
+		},
+		:invoice => {
+			'00' => :paid,
+			'01' => :pending,
+			'02' => :pending,
+			'03' => :pending,
+			'04' => :pending,
+			'05' => :paid,
+			'06' => :pending   
+		},
+		:credit_card => {
+			'00' => :paid,
+			'01' => :pending,
+			'02' => :pending,
+			'03' => :pending      
+		}
+	}
+	
+	PAYMENT_METHODS = {
+		'00' => :undefined,
+		'01' => :online_transfer,
+		'02' => :invoice,    
+		'03' => :credit_card
+	}
+	
+	def gera_dados(params)
+		default = {codigo_da_empresa: nil,
+		pedido: nil,
+		valor: nil,
+		observacao: nil,
+		chave: nil,
+		nome_do_sacado: nil,
+		codigo_da_inscricao: nil,
+		numero_da_inscricao: nil,
+		endereco_do_sacado: nil,
+		bairro_do_sacado: nil,
+		cep_do_sacado: nil,
+		cidade_do_sacado: nil,
+		estado_do_sacado: nil,
+		data_de_vencimento: nil,
+		url_de_retorno: nil,
+		obs_adicional1: nil,
+		obs_adicional2: nil,
+		obs_adicional3: nil}
 
-  def dcript_transaction_notify(dados)
-    cripto = ItauCripto.new
-    cripto.decripto(dados, config['chave'])    
-  end
-  
-  def get_invoice_details(invoice_id)
-    cripto = ItauCripto.new
-    token = cripto.gera_consulta(config['codigo_empresa'], invoice_id.to_s, '1',config['chave'])
-    xml = open("https://shopline.itau.com.br/shopline/consulta.aspx?DC=#{token}").read
+		params = default.merge!(params) { |key, v1, v2| v2 }
 
-    treat_itau_data xml    
-  end
-  
-  private
-    def treat_itau_data(dados)
-      treated_data = {}
+		params[:codigo_da_inscricao] = (params[:codigo_da_inscricao] == :cpf) ? "01" : "02"
+		params[:codigo_da_empresa] = config["codigo_empresa"]
+		params[:chave] = config["chave"]
+		params[:valor] = number_to_currency(params[:valor], :unit => "", :separator => ",", :delimiter => "")
+		params[:data_de_vencimento] = params[:data_de_vencimento].strftime('%d%m%Y')
 
-      response = REXML::Document.new(dados)
-      params  = response.elements['consulta'].elements['PARAMETER']
-      params.elements.each do |param|
-        treated_data[param.attributes['ID']] = param.attributes['VALUE']
-      end
-      
-      new_result = {}
-      treated_data.each do |field, value|
-        value = PAYMENT_METHODS[value] if field == 'tipPag'
-        value = STATUS[PAYMENT_METHODS[treated_data['tipPag']]][value] if field == 'sitPag'        
+		cripto = ItauCripto.new
+		cripto.gera_dados(params.values.collect(&:to_s))
+	end
 
-        new_result[field] = value
-      end
-      
-      new_result
-    end
-  
-    def config
-      {'codigo_empresa' => ENV["ITAU_ENTERPRISE"], 'chave' => ENV["ITAU_KEY"]}
-    end
+	def dcript_transaction_notify(dados)
+		cripto = ItauCripto.new
+		cripto.decripto(dados, config['chave'])    
+	end
+	
+	def get_invoice_details(invoice_id)
+		cripto = ItauCripto.new
+		token = cripto.gera_consulta(config['codigo_empresa'], invoice_id.to_s, '1',config['chave'])
+		xml = open("https://shopline.itau.com.br/shopline/consulta.aspx?DC=#{token}").read
+
+		treat_itau_data xml    
+	end
+	
+	private
+		def treat_itau_data(dados)
+			treated_data = {}
+
+			response = REXML::Document.new(dados)
+			params  = response.elements['consulta'].elements['PARAMETER']
+			params.elements.each do |param|
+				treated_data[param.attributes['ID']] = param.attributes['VALUE']
+			end
+			
+			new_result = {}
+			treated_data.each do |field, value|
+				value = PAYMENT_METHODS[value] if field == 'tipPag'
+				value = STATUS[PAYMENT_METHODS[treated_data['tipPag']]][value] if field == 'sitPag'        
+
+				new_result[field] = value
+			end
+			
+			new_result
+		end
+	
+		def config
+			{'codigo_empresa' => ENV["ITAU_ENTERPRISE"], 'chave' => ENV["ITAU_KEY"]}
+		end
 end
